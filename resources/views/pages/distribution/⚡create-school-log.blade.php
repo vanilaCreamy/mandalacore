94cd680c9be3a9b38478c2ece6bfd3e190c15af0
Dani Nugraha <nugrahadani563@gmail.com>
2026-03-26 21:37:34 +0700

    fix enum namespace

--- resources/views/pages/distribution/⚡create-school-log.blade.php
+++ resources/views/pages/distribution/⚡create-school-log.blade.php
@@ -1,8 +1,8 @@
 <?php
 
 use Livewire\Component;
-use App\Enum\DriverCategory;
-use App\Enum\DriverFlow;
+use App\enum\DriverCategory;
+use App\enum\DriverFlow;
 use App\Models\SchoolDelivery;
 use App\Models\School;
 use App\Models\DriverLocation;
